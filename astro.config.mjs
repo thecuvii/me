c5010b548fa5c35ef98fe2227f8fc2fd65afebdd
AqingCyan <aqingcyan@gmail.com>
2023-02-24 21:44:53 +0800

    style: layout change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
 import { defineConfig } from 'astro/config'
-
-// https://astro.build/config
 import react from '@astrojs/react'
+import mdx from '@astrojs/mdx'
+import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), mdx()],
   markdown: {
     syntaxHighlight: 'prism',
     rehypePlugins: ['rehype-slug', ['rehype-autolink-headings', { behavior: 'prepend' }]],
+  },
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+      }),
+    ],
   },
 })
